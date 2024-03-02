241d07633a2749bb10c2ca9248522648d0cdc2b8
MeroFuruya <80584154+MeroFuruya@users.noreply.github.com>
2024-03-02 22:39:14 +0100

    Add new files and update configurations

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { a } from '@repo/types';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
-
-const b: a = 'x';

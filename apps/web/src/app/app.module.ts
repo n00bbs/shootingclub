31931c6878e2d13683cfa16c4933876d0590d802
MeroFuruya <80584154+MeroFuruya@users.noreply.github.com>
2024-03-05 07:45:23 +0100

    Refactor app module and add SC base layout

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
-// Material UI
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-const materialModules = [MatSlideToggleModule];
+// Base Modules
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+const baseModules = [
+  BrowserModule,
+  CommonModule,
+  FormsModule,
+  RouterModule.forRoot(routes),
+  RouterOutlet,
+  BrowserAnimationsModule,
+];
+
+// // Material UI
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// const materialModules = [MatSlideToggleModule];
+
+// SC ui
+import { ScBaseLayoutModule } from '@repo/ui';
+const scModules = [ScBaseLayoutModule];
+
+// App Components
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+const appComponents = undefined;
 
 @NgModule({
-  imports: [RouterOutlet, ...materialModules],
+  imports: [...baseModules, ...scModules],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,1 +1,2 @@
-export const a = 1;
+// layouts
+export * from './layout/sc-base';

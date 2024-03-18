f8704489c1cf6b4cc623921fc254fe63d775a581
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-03-18 11:53:24 +0100

    Add NestJS configuration and TypeORM setup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.ENV_FILE_PATH || '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: entities,
+      logger: 'advanced-console',
+      logging: 'all',
+      applicationName: 'shootingclub-api',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/app/entities/userDepartmentChange.entity.ts
+++ apps/api/src/app/entities/userDepartmentChange.entity.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
-import { PrimaryGeneratedColumn } from 'typeorm/browser';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { DepartmentEntity } from './department.entity';
 import { UserDepartmentChangeTypeEntity } from './userDepartmentChangeType.entity';

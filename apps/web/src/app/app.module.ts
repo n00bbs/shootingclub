7b7a5c45cc59fcf88d7391736034678c7a5c7717
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-03-21 11:22:38 +0100

    Update VSCode settings and add default layout for web app
    Co-authored-by: CaptainException <CaptainException@users.noreply.github.com>

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 const baseModules = [
   BrowserModule,
@@ -16,14 +18,25 @@ const baseModules = [
 ];
 
 // Material UI
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-const materialModules = [MatSlideToggleModule];
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+const materialModules = [
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+];
 
 // App Components
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-const appComponents = [AppComponent];
+import { DefaultLayoutComponent } from './layouts/default/default.layout';
+const appComponents = [AppComponent, DefaultLayoutComponent];
 
 @NgModule({
   imports: [...baseModules, ...materialModules],
